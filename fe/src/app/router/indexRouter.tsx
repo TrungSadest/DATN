235af1081ec6c97a580b72151e7713c127951f77
TrungSadest <trungpnph29130@fpt.edu.vn>
2024-12-12 19:38:47 +0700

    init fe

---
+++ fe/src/app/router/indexRouter.tsx
@@ -0,0 +1,12 @@
+import Layout from "../layout/Layout";
+import Dashboard from "../page/dashboard/Dashboard";
+import Profile from "../page/profile/Profile";
+
+export const indexRouter: any = {
+  path: "/",
+  element: <Layout />,
+  children: [
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/profile', element: <Profile /> },
+  ],
+};
